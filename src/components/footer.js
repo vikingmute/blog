c94c23632701cbe48723414b6aeef4e262271139
Viking Zhang <vikingmute@gmail.com>
2020-07-21 15:16:35 +0800

    1 Add exchange link in the footer 2 Fix eslint error a links without rel 3 Fix elsint erros image without alt text

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,23 +15,29 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div>
-      © {new Date().getFullYear()}, 由
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a> 创建
+        <p>
+          © {new Date().getFullYear()}, 由
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a> 创建
+        </p>
+        <ul style={{ display: 'flex'}}>
+          <li>友情链接：</li>
+          <li><a href="https://kalasearch.cn/" target="_blank" rel="noopener noreferrer">卡拉搜索</a></li>
+        </ul>
       </div>
       <div>
         <p>我除了写文章，还拍关于独立开发者的视频，欢迎关注</p>
         <div className={styles.cols}>
           <ul style={{ position: 'relative'}}>
-            <img src={gzh} style={picStyles} className={styles.gzh}/>
-            <li><a href="#" target="_blank" onClick={handleClick}>微信公众号，{showText}</a></li>
-            <li><a href="https://www.zhihu.com/people/vikingmute" target="_blank">知乎专栏：程序员的乌托邦</a></li>
-            <li><a href="https://space.bilibili.com/480241067" target="_blank">B站：独立开发者 Viking</a></li>
+            <img src={gzh} style={picStyles} className={styles.gzh} alt="程序员乌托邦 微信公众号"/>
+            <li><button onClick={handleClick}>微信公众号，{showText}</button></li>
+            <li><a href="https://www.zhihu.com/people/vikingmute" target="_blank" rel="noopener noreferrer">知乎专栏：程序员的乌托邦</a></li>
+            <li><a href="https://space.bilibili.com/480241067" target="_blank" rel="noopener noreferrer">B站：独立开发者 Viking</a></li>
           </ul>
           <ul>
-          <li><a href="https://www.youtube.com/channel/UCm5Z1WsCPhxl_NznRK0tvEw" target="_blank">Youtube： 程序员的乌托邦</a></li>
-          <li><a href="https://www.github.com/vikingmute/" target="_blank">Github：vikingmute</a></li>
-          <li><a href="https://twitter.com/vikingmute" target="_blank">Twitter：Viking</a></li>
+          <li><a href="https://www.youtube.com/channel/UCm5Z1WsCPhxl_NznRK0tvEw" target="_blank" rel="noopener noreferrer">Youtube： 程序员的乌托邦</a></li>
+          <li><a href="https://www.github.com/vikingmute/" target="_blank" rel="noopener noreferrer">Github：vikingmute</a></li>
+          <li><a href="https://twitter.com/vikingmute" target="_blank" rel="noopener noreferrer">Twitter：Viking</a></li>
           </ul>
         </div>
       </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -24,14 +24,14 @@ const AboutPage = (props) =>  {
           <p>我叫张轩，ID 是 Viking，我是一名前端开发工程师，同时也是一名独立开发者，目前生活在上海。我水平一般，但是在教学上有一点天赋，我在 2016 年和哥们出过一本书，从18年下半年到现在，在慕课网出品了五套课程。</p>
           <h3 style={centerStyle}>我出品的课程 - 收费</h3>
           <ul>
-            <li><a href="https://coding.imooc.com/class/428.html" target="_blank">React+TypeScript高仿AntDesign开发企业级UI组件库</a></li>
-            <li><a href="https://coding.imooc.com/class/384.html" target="_blank">Electron+React+七牛云 实战跨平台桌面应用</a></li>
-            <li><a href="https://coding.imooc.com/class/302.html" target="_blank">React16组件化+测试+全流程实战“在线账本”项目</a></li>
+            <li><a href="https://coding.imooc.com/class/428.html" target="_blank" rel="noopener noreferrer">React+TypeScript高仿AntDesign开发企业级UI组件库</a></li>
+            <li><a href="https://coding.imooc.com/class/384.html" target="_blank" rel="noopener noreferrer">Electron+React+七牛云 实战跨平台桌面应用</a></li>
+            <li><a href="https://coding.imooc.com/class/302.html" target="_blank" rel="noopener noreferrer">React16组件化+测试+全流程实战“在线账本”项目</a></li>
           </ul>
           <h3 style={centerStyle}>我出品的课程 - 免费</h3>
           <ul>
-            <li><a href="https://www.imooc.com/learn/1198" target="_blank">Electron开发仿网易云音乐播放器</a></li>
-            <li><a href="https://www.imooc.com/learn/1045" target="_blank">基于实例的 React16 傻瓜课程</a></li>
+            <li><a href="https://www.imooc.com/learn/1198" target="_blank" rel="noopener noreferrer">Electron开发仿网易云音乐播放器</a></li>
+            <li><a href="https://www.imooc.com/learn/1045" target="_blank" rel="noopener noreferrer">基于实例的 React16 傻瓜课程</a></li>
           </ul>
           <h3 style={centerStyle}>Opensale 计划</h3>
           <p>
@@ -39,26 +39,26 @@ const AboutPage = (props) =>  {
             意在帮助自己或者任何对自由职业感兴趣的人，追踪每个月的收入。
           </p>
           <hr/>
-          <h3 style={centerStyle}>我和 <a href="https://nighca.me/" target="_blank"> 杨寒星</a>一起出版的书</h3>
+          <h3 style={centerStyle}>我和 <a href="https://nighca.me/" target="_blank" rel="noopener noreferrer"> 杨寒星</a>一起出版的书</h3>
           <p>
             机缘巧合，我关于 Webpack 的一篇文章在知乎上火了起来，电子工业出版社编辑找到我，希望我写一本书，我现在的水平也不够写书，更别说当时。
             但是我当时正好处在 gap month，用写一本书的机遇来提高自己，我自己认为是不错的机会，所以我找来了我认为比较牛的一个同事，互相督促和校对，所以也就开始合作。
             书的名字很长：《React全栈：Redux+Flux+webpack+Babel整合开发》，这是编辑的意思，把当时所有 trending 的技术词汇都扔到一个标题里面去。下面的链接，是以比较客观的评价来排序的，你可以自行选择。
           </p>
           <ul>
-            <li><a href="https://book.douban.com/subject/26901067/" target="_blank">本书在豆瓣</a></li>
-            <li><a href="https://www.amazon.cn/dp/B06Y2MY6Z8/" target="_blank">本书在亚马逊</a></li>
-            <li><a href="https://item.jd.com/12052766.html" target="_blank">本书在京东</a></li>
+            <li><a href="https://book.douban.com/subject/26901067/" target="_blank" rel="noopener noreferrer">本书在豆瓣</a></li>
+            <li><a href="https://www.amazon.cn/dp/B06Y2MY6Z8/" target="_blank" rel="noopener noreferrer">本书在亚马逊</a></li>
+            <li><a href="https://item.jd.com/12052766.html" target="_blank" rel="noopener noreferrer">本书在京东</a></li>
           </ul>
           <h3 style={centerStyle}>和我聊聊</h3>
           <p>
             如果你对本网站的文章感兴趣，想接受推送，你可以关注下图这个微信公众号。
             <br/>
-            <img src={gzh} width="160px" style={imageCenterStyle}/>
+            <img src={gzh} width="160px" style={imageCenterStyle} alt="程序员乌托邦 微信公众号"/>
 
             如果你想和我约饭或者随便聊聊，可以通过下图这个微信加我。
             <br/>
-            <img src={weChat} width="150px" style={imageCenterStyle}/>
+            <img src={weChat} width="150px" style={imageCenterStyle} alt="vikingmute 微信二维码"/>
 
           </p>
           <div style={{maxWidth: '680px', margin: '2rem auto'}}>
